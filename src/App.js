2679d0f6de387ba7094e40ebd9ef7df49dd2ea05
azaleas <azaleas@2die4.com>
2017-01-30 23:13:35 +0400

    added new recipe addition, cancel button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 import './App.css';
 
 const data = [
   {
+    id: "recipe-1",
     recipe_name: "Peanut Butter and Jelly Sandwich",
     ingredients: [
       "Bread (usually one or two slices per sandwich)",
       "Peanut Butter",
       "Jelly or Jam",
     ],
-    instructions: [
-      "Spread peanut butter evenly onto one slice of bread using a knife.",
-      "Spread jelly or jam evenly onto the other slice of bread.",
-      "Press the two slices of bread together.",
-      "Cut the sandwich",
-      "Enjoy your easy and yummy looking sandwich!"
-    ],
+    instructions: "Spread peanut butter evenly onto one slice of bread using a knife. Spread jelly or jam evenly onto the other slice of bread. Press the two slices of bread together. Cut the sandwich. Enjoy your easy and yummy looking sandwich!",
   }
 ];
 
@@ -26,76 +23,106 @@ class RecipeForm extends Component {
   
     this.state = {
       ingredients: 1,
-      instructions: 1,
+      recipeData: {},
+      fieldError: {},
     };
   }
 
-  addElement = (event) => {
-    let element = event.target.name;
+  addElement = (event) => {    
+    this.setState({
+      ingredients: this.state.ingredients + 1,
+    });
+  }
 
-    if(element === "instruction"){
+  deleteElement = (event) => {
+    if(this.state.ingredients > 1){
       this.setState({
-        instructions: this.state.instructions + 1,
+        ingredients: this.state.ingredients - 1,
+      })
+    }
+  }
+
+  onChange = (event) => {
+    let elementName = event.target.name;
+    if(elementName === "recipeName"){
+      let recipeName = {recipe_name: event.target.value};
+      this.setState({
+        recipeData: Object.assign(this.state.recipeData, recipeName),
       });
     }
-    else{
+    else if(elementName === "instructions"){
+      let instructions = {instructions: event.target.value};
       this.setState({
-        ingredients: this.state.ingredients + 1,
+        recipeData: Object.assign(this.state.recipeData, instructions),
+      });
+    }
+    else if(elementName.indexOf("ingredient_") !== -1){
+      let ingredients = this.state.recipeData.ingredients || [];
+      let elementId = elementName.replace('ingredient_', '') - 1;
+      ingredients[elementId] = event.target.value;
+      let ingredientsObject = {ingredients};
+      this.setState({
+        recipeData: Object.assign(this.state.recipeData, ingredientsObject),
       });
     }
   }
 
-  deleteElement = (event) => {
-    let element = event.target.name;
+  formSubmit = (event) => {
+    event.preventDefault();
+    let totalData = Object.assign({}, this.state.recipeData);
+    const fieldError = this.validate(totalData);
+    this.setState({fieldError: fieldError});
+    if(Object.keys(fieldError).length) return;
+    this.props.formSubmit(totalData);
+    this.setState({recipeData: {}});
+  }
 
-    if(element === "instruction"){
-      if(this.state.instructions >= 1){
-        this.setState({
-          instructions: this.state.instructions - 1,
-        })
-      }
+  validate = (data) => {
+    const errors = {};
+    if(!data.recipe_name || typeof(data.recipe_name) === "undefined") errors.recipe_name = true;
+    if(!data.instructions || typeof(data.instructions) === "undefined") errors.instructions = true;
+    let ingredientErrors = [];
+    for(let ref in this.refs) {
+        if(ref.indexOf("ingredient_") !== -1){
+          let inputValue = ReactDOM.findDOMNode(this.refs[ref]).value;
+          if(inputValue === ""){
+            ingredientErrors.push(true);
+          }
+          else{
+            ingredientErrors.push(false);
+          }
+        }
     }
-    else{
-      if(this.state.ingredients >= 1){
-        this.setState({
-          ingredients: this.state.ingredients - 1,
-        })
-      }
-    }
+
+    errors.ingredients = ingredientErrors;
+    
+    return errors;
   }
 
   render(){
     let ingredients = [];
-    let instructions = [];
     for (let indexNumber = 0; indexNumber < this.state.ingredients; indexNumber++) {
       ingredients.push(
         <input 
           type="text" 
-          className="form-control" 
-          name={"ingredient_" + indexNumber+1}
-          key={"ingredient_" + indexNumber+1} 
-          placeholder="Ingredient Name" />
+          className={"form-control " + ((typeof this.state.fieldError.ingredients !== "undefined" && this.state.fieldError.ingredients[indexNumber]) ? "has-error" : "")}
+          name={"ingredient_" + (indexNumber+1)}
+          key={"ingredient_" + (indexNumber+1)} 
+          ref={"ingredient_" + (indexNumber+1)} 
+          placeholder="Ingredient Name" 
+          onChange={this.onChange} />
       )
     }
 
-    for (let indexNumber = 0; indexNumber < this.state.instructions; indexNumber++) {
-      instructions.push(
-        <input 
-          type="text" 
-          className="form-control" 
-          name={"instruction_" + indexNumber+1}
-          key={"instruction_" + indexNumber+1} 
-          placeholder="Instruction" />
-      )
-    }
     return (
       <div className="RecipeForm">
-        <form>
+        <form onSubmit={this.formSubmit}>
           <input 
             type="text" 
-            className="form-control" 
+            className={"form-control " + (this.state.fieldError.recipe_name ? "has-error" : "")}
             name="recipeName" 
-            placeholder="Recipe Name" />
+            placeholder="Recipe Name"
+            onChange={this.onChange} />
           <hr/>
           {ingredients}
           <a 
@@ -113,20 +140,11 @@ class RecipeForm extends Component {
             Delete
           </a>
           <hr/>
-          {instructions}
-          <a 
-            className="add btn btn-primary"
-            onClick={this.addElement}
-            name="instruction">
-            Add
-          </a>
-          <a 
-            className="add btn btn-primary"
-            onClick={this.deleteElement}
-            name="instruction"
-          >
-            Delete
-          </a>
+          <textarea 
+          className={"form-control " + (this.state.fieldError.instructions ? "has-error" : "")}
+          name="instructions"
+          placeholder="Instructions"
+          onChange={this.onChange} />
           <hr/>
           <button type="submit" className="btn btn-success">Submit</button>
         </form>
@@ -141,13 +159,17 @@ const RecipesList = (props) => {
       {
         props.data.map((el, index) => {
           return(
-            <div key={"recipe" + index} className="RecipeBox">
+            <div 
+              key={"recipe" + el.id} 
+              className="RecipeBox"
+              >
               <a href="#" 
+                id={el.id}
                 className="recipe_title"
                 onClick={props.recipeStatus}>
                 {el.recipe_name}
               </a>
-              <div className={"recipe_details " + (props.recipeOpen ? "collapse in" : "collapse")}>
+              <div className={"recipe_details " + (props.activeElementId === el.id ? "collapse in" : "collapse")}>
                 <h3>Ingredients</h3>
                 <ol>
                   {
@@ -161,17 +183,7 @@ const RecipesList = (props) => {
                   }
                 </ol>
                 <h3>Instructions</h3>
-                <ol>
-                  {
-                    el.instructions.map((el, index) => {
-                      return (
-                        <li key={"instruction" + index}>
-                          {el}
-                        </li>
-                      )     
-                    })
-                  }
-                </ol>
+                <p>{el.instructions}</p>
               </div>
             </div>
           );
@@ -188,7 +200,8 @@ class App extends Component {
     this.state = {
       new_recipe: false,
       data: data,
-      recipeOpen: false,
+      activeElementId: '',
+      activeButton: 'add',
     };
   }
 
@@ -199,22 +212,33 @@ class App extends Component {
     });
   }
 
-  recipeStatus = (event) => {
-    let recipeOpen = this.state.recipeOpen;
-    if(recipeOpen){
-      recipeOpen = false;
-    }
-    else{
-      recipeOpen = true;
-    }
+  formSubmit = (data) => {
+    let newId = this.state.data.length + 1;
+    let dataWithId = Object.assign({}, data, {id: "recipe-" + newId});
     this.setState({
-      recipeOpen,
+      data: this.state.data.concat(dataWithId),
+      new_recipe: false,
+    });
+    localStorage.setItem('recipe-data', JSON.stringify(this.state.data.concat(dataWithId)));
+  }
+
+  recipeStatus = (event) => {
+    this.setState({
+      activeElementId: event.target.id,
     })
   }
 
   addRecipe = (event) => {
     this.setState({
       new_recipe: true,
+      activeButton: 'cancel',
+    })
+  }
+
+  cancelRecipe = (event) => {
+    this.setState({
+      new_recipe: false,
+      activeButton: 'add',
     })
   }
 
@@ -223,21 +247,28 @@ class App extends Component {
       <div className="App container">
         <h3 className="bg-primary title">Recipe Box</h3>
         <button 
-          className="btn btn-success"
+          className={"btn btn-primary " + (this.state.activeButton === "add" ? "": "hidden")}
           onClick={this.addRecipe}
         >
           Add Recipe
         </button>
+        <button 
+          className={"btn btn-danger " + (this.state.activeButton === "cancel" ? "": "hidden")}
+          onClick={this.cancelRecipe}
+        >
+          Cancel
+        </button>
         {
           this.state.new_recipe
           ? (
-            <RecipeForm />
+            <RecipeForm 
+              formSubmit={this.formSubmit}/>
           )
           : (
             <RecipesList 
               data={this.state.data}
               recipeStatus={this.recipeStatus}
-              recipeOpen={this.state.recipeOpen}/>
+              activeElementId={this.state.activeElementId}/>
           )
         }
       </div>
